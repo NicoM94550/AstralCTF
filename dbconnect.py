7c8686d81ae6bcacdee3347e4fb3aed7ae202fe0
Nicolas Macchioni <NicoM94550@yahoo.com>
2018-11-01 16:46:45 -0400

    Update dbconnect.py
    Redaction

--- dbconnect.py
+++ dbconnect.py
@@ -1,6 +1,6 @@
 import MySQLdb
 
 def connection():
-	conn = MySQLdb.connect(host="localhost", user="root", passwd="GuE55/thLs/1492", db="AstralCTF")
+	conn = MySQLdb.connect(host="REDACTED", user="REDACTED", passwd="REDACTED", db="REDACTED")
 	c = conn.cursor()
 	return c,conn
